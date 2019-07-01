9e80d5545dfae4c1f554b3c7e2197beb4e418a77
Kithraya <46088893+Kithraya@users.noreply.github.com>
2019-07-01 12:56:38 -0400

    Update detect-ie.js

--- detect-ie.js
+++ detect-ie.js
@@ -2,7 +2,7 @@
 
 var IE = (function(win, undefined) { // keep the global namespace unpolluted by wrapping in an IIFE
 	
-	var obj = { engine: null, 'documentMode': document.documentMode }
+	var obj = { engine: undefined, 'documentMode': document.documentMode }
 
 	var is_IE11 = !!( !('currentScript' in document) && window.msCrypto);
 	
@@ -12,10 +12,12 @@ var IE = (function(win, undefined) { // keep the global namespace unpolluted by 
 	// https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
 	// And document.currentScript is not defined in any version of IE, but all versions of Edge support it.
 	// so we combine the checks to future proof it.
-	// however, if documentMode has been changed from the default, this will return false.
+	// however, if documentMode has been changed from the default, this will return false,
+	// but @_jscript_version will return "11".
 	
 	
 	var IE_number = (function() {
+		// using handy conditional compilation which executes in IE only, and does not change with document mode.
 		var version = new Function("/*@cc_on return @_jscript_version; @*/")();
 		// "5.6/7": IE6, "5.7": IE7, "5.8": IE8, "9":IE9, "10": IE10, "11": IE11 with an older document mode.
 		// undefined if not IE, or regular IE11
@@ -25,12 +27,12 @@ var IE = (function(win, undefined) { // keep the global namespace unpolluted by 
 	
 	// check for IE11
 	// if IE11 is in an older document mode, @_jscript_version will be exposed and we can use that
-	// otherwise, the initial check will return true
+	// otherwise, the initial IE11 check will return true
 	
 	if (is_IE11 || parseInt(IE_number,10) === 11 ){
 		obj.engine = 11;
 	} else {
-		obj.engine = (IE_number) ? IE_number : 'not-IE';
+		obj.engine = (IE_number) ? IE_number : undefined;
 	}
 
 	return obj;
