9bcc349c0a3731a1475232c4685cd53258053e91
Kithraya <46088893+Kithraya@users.noreply.github.com>
2020-06-17 08:41:22 +0100

    Update detect-ie.js

--- detect-ie.js
+++ detect-ie.js
@@ -5,10 +5,11 @@ window.isIE = (function(win, doc, undefined) {
 
 	// keep the global namespace unpolluted by wrapping in an IIFE
 	
-	var jscript = { engine: null, 'documentMode': document.documentMode };
+	var jscript = { engine: null, 'documentMode': document.documentMode, version: null };
 
-	var is_default_IE11 = !!( !('currentScript' in document) && window.msCrypto );
+	var is_default_IE11 = !!( !document['currentScript'] && window.msCrypto );
 	
+	var map = { '5.5': 5.5, '5.6': 6, '5.7': 7, '5.8': 8, '9': 9, '10': 11, '11': 11 };
 	
 	// msCrypto (with the prefix) is only defined in IE11 in IE11 document mode (for now). (Yes, I checked via Browserstack)
 	// Source: https://developer.mozilla.org/en-US/docs/Web/API/Window/crypto 
@@ -24,22 +25,23 @@ window.isIE = (function(win, doc, undefined) {
 	/// Values: IE6|7: "5.6" / "5.7", IE8: "5.8", IE9: "9", IE10: "10", IE11*: "11", IE11: `undefined`, NOT IE: `undefined`; 
 	
 	var jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*/")() ) || undefined; 
-	
+
 	///  Workaround Test for Windows Service Pack Update (IE6 / 7)
 	if (jscript_version === 5.7 && !window.XMLHttpRequest) { jscript_version = 5.6 }
-	
+
 	// if IE11 is in an older document mode, @_jscript_version will be exposed and we can use that
 	// otherwise, the initial IE11 check will return true. If neither are true, it's IE < 11 or not IE.
 	
 	if ( is_default_IE11 || jscript_version === 11 ){
 		
-		jscript.engine = 11;
+		jscript.engine = jscript.version = 11;
 		
-	} else { // browser is old IE or not IE
+	} else { // browser is IE < 11 or not IE
 	
 		if (!jscript_version) { return false } 	
 		
 		jscript.engine = jscript_version; 
+		jscript.version = map[jscript_version] || 'wtf';
 		
 	}
 
