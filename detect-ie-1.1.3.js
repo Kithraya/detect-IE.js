307fcf6a16adc6dc891a9c73fa45fd5cee93e1a4
Kithraya <46088893+Kithraya@users.noreply.github.com>
2020-07-24 20:56:43 +0100

    Update and rename detect-ie.js to detect-ie-1.1.3.js

--- detect-ie-1.1.3.js
+++ detect-ie-1.1.3.js
@@ -24,24 +24,24 @@ window.isIE = (function(win, doc, undefined) {
 	// Conditional compilation executes in IE only, and does not change with document mode.
 	/// Values: IE6|7: "5.6" / "5.7", IE8: "5.8", IE9: "9", IE10: "10", IE11*: "11", IE11: `undefined`, NOT IE: `undefined`; 
 	
-	var jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*/")() ) || undefined; 
+	var _jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*/")() ) || undefined; 
 
 	///  Workaround Test for Windows Service Pack Update (IE6 / 7)
-	if (jscript_version === 5.7 && !window.XMLHttpRequest) { jscript_version = 5.6 }
+	if (_jscript_version === 5.7 && !window.XMLHttpRequest) { _jscript_version = 5.6 }
 
 	// if IE11 is in an older document mode, @_jscript_version will be exposed and we can use that
 	// otherwise, the initial IE11 check will return true. If neither are true, it's IE < 11 or not IE.
 	
-	if ( is_default_IE11 || jscript_version === 11 ){
+	if ( is_default_IE11 || _jscript_version === 11 ){
 		
 		jscript.engine = jscript.version = 11;
 		
 	} else { // browser is IE < 11 or not IE
 	
-		if (!jscript_version) { return false } 	
+		if (!_jscript_version) { return false } 	
 		
-		jscript.engine = jscript_version; 
-		jscript.version = map[jscript_version] || 'wtf';
+		jscript.engine = _jscript_version; 
+		jscript.version = map[ String(_jscript_version) ] || 'wtf';
 		
 	}
 
