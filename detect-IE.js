c5469fd3370fc63e15b704e0eb7cad711ed46cff
Kithraya <46088893+Kithraya@users.noreply.github.com>
2020-08-17 22:11:38 +0100

    Update detect-IE.js

--- detect-IE.js
+++ detect-IE.js
@@ -2,9 +2,7 @@
 
 window.isIE = (function(win, doc, undefined) {
 	
-	var envir = { jscript: null, documentMode: document.documentMode, browser: null };
-
-	var is_default_IE11 = !!window.msCrypto;
+	var envir = {}, is_default_IE11 = !!window.msCrypto;
 	
 	var mapIE = { '5': 5, '5.5': 5.5, '5.6': 6, '5.7': 7, '5.8': 8, '9': 9, '10': 11, '11': 11 }; // browser versions
 	
@@ -18,11 +16,11 @@ window.isIE = (function(win, doc, undefined) {
 	// By default, IE11 does not interpret conditional compilation, so `@_jscript_version` is `undefined`. 
 	// But if IE11's documentMode has been changed from the default, "is_default_IE11" will return `false`,
 	// and `@_jscript_version` will return "11". (Yep, textbook Microsoft)
-
+ 
 	/// Values: IE6|7: "5.6" / "5.7", IE8: "5.8", IE9: "9", IE10: "10", IE11*: "11", IE11: `undefined`, NOT IE: `undefined`; 
 	
 	var jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*/")() ) || (is_default_IE11 ? 11 : NaN);
-	
+ 
 	if (!jscript_version) { return false }
 
 	///  Workaround Test for Windows Service Pack Update (IE6 / 7). Document mode wasnt introduced until IE8, so this check works fine.
@@ -30,7 +28,11 @@ window.isIE = (function(win, doc, undefined) {
 	
 	envir.jscript = jscript_version;
 	
+	envir.documentMode = document.documentMode;
+	
 	envir.browser = mapIE[ String(jscript_version) ] || jscript_version;
+	
+	envir.is_default_IE11 = is_default_IE11;
 
 	return envir;
 
