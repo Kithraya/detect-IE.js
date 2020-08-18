84c2741ed0ea7b65aeb62d4c2ce02b08b59f6138
Kithraya <46088893+Kithraya@users.noreply.github.com>
2020-08-18 03:21:17 +0100

    Update detect-IE.js

--- detect-IE.js
+++ detect-IE.js
@@ -1,10 +1,10 @@
-// Detect IE without User-agent hacks. | Supports minification. | MIT license | Version 1.1.6 | https://github.com/Kithraya/detect-IE
+// Detect IE without User-agent hacks. | Supports minification. | MIT license. | Version 1.1.6 | https://github.com/Kithraya/detect-IE
 
 window.isIE = (function(win, doc, undefined) {
 	
 	var envir, is_default_IE11 = !!window.msCrypto;
 	
-	var mapIE = { '5': 5, '5.5': 5.5, '5.6': 6, '5.7': 7, '5.8': 8, '9': 9, '10': 11, '11': 11 }; // browser versions
+	var mapIE = { '5': 5, '5.5': 5.5, '5.6': 6, '5.7': 7, '5.8': 8, '9': 9, '10': 11, '11': 11 }; // IE browser versions
 	
 	// Unknown values for IE < 5. Estimates: '1': 3, '3': 4, '5.01': 5.01, but anything below IE 5.5 is bs anyway.
 	// msCrypto (with the prefix) is only defined in IE11 in IE11 document mode. (Yes, I checked via Browserstack)
@@ -19,7 +19,7 @@ window.isIE = (function(win, doc, undefined) {
  
 	/// Values: IE6|7: "5.6" / "5.7", IE8: "5.8", IE9: "9", IE10: "10", IE11*: "11", IE11: `undefined`, NOT IE: `undefined`; 
 	
-	var jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*/")() ) || (is_default_IE11 ? 11 : NaN);
+	var jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*\/")() ) || (is_default_IE11 ? 11 : NaN);
  
 	if (!jscript_version) { return false }
 
@@ -27,7 +27,7 @@ window.isIE = (function(win, doc, undefined) {
 	if (jscript_version === 5.7 && !window.XMLHttpRequest) { jscript_version = 5.6 }
 	
 	envir = { jscript: jscript_version, documentMode: document.documentMode, is_default_IE11: is_default_IE11 };
-	
+		
 	envir.browser = mapIE[ String(jscript_version) ] || jscript_version;
 	
 	envir[envir.browser] = true;
